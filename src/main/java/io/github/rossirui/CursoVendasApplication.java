624336acffd1ad9cf3b64e0cc9113fb727975bd9
rossirui <rui.rossi@gmail.com>
2020-07-18 21:52:22 -0300

    Criacao da aplicacao Spring boot

---
+++ src/main/java/io/github/rossirui/CursoVendasApplication.java
@@ -0,0 +1,12 @@
+package io.github.rossirui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoVendasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CursoVendasApplication.class, args);
+    }
+}
