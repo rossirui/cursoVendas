725cd5c755814fc2791f5b2a632bcec4d5d77342
Rui Rossi <rui.rossi@gmail.com>
2020-07-19 16:35:16 -0300

    Instanciamento hardcore

--- src/main/java/io/github/rossirui/CursoVendasApplication.java
+++ src/main/java/io/github/rossirui/CursoVendasApplication.java
@@ -1,12 +1,29 @@
 package io.github.rossirui;
 
+import io.github.rossirui.domain.entities.Categoria;
+import io.github.rossirui.domain.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursoVendasApplication {
+public class CursoVendasApplication implements CommandLineRunner {
+
+    @Autowired
+    CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursoVendasApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria categoria1 = new Categoria(null, "Informática");
+        Categoria categoria2 = new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
     }
 }
