c355f2524b41a30b4b9e42956d9abe4bffa4f582
Rui Rossi <rui.rossi@gmail.com>
2020-07-20 12:51:40 -0300

    Classe Produto e associacao Many To Many

--- src/main/java/io/github/rossirui/CursoVendasApplication.java
+++ src/main/java/io/github/rossirui/CursoVendasApplication.java
@@ -1,7 +1,9 @@
 package io.github.rossirui;
 
 import io.github.rossirui.domain.entities.Categoria;
+import io.github.rossirui.domain.entities.Produto;
 import io.github.rossirui.domain.repositories.CategoriaRepository;
+import io.github.rossirui.domain.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +17,9 @@ public class CursoVendasApplication implements CommandLineRunner {
     @Autowired
     CategoriaRepository categoriaRepository;
 
+    @Autowired
+    ProdutoRepository produtoRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(CursoVendasApplication.class, args);
     }
@@ -24,6 +29,18 @@ public class CursoVendasApplication implements CommandLineRunner {
         Categoria categoria1 = new Categoria(null, "Informática");
         Categoria categoria2 = new Categoria(null, "Escritório");
 
+        Produto produto1 = new Produto(null, "Computador", 2000.00);
+        Produto produto2 = new Produto(null, "Impressora", 800.00);
+        Produto produto3 = new Produto(null, "Mouse", 80.00);
+
+        categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+        categoria2.getProdutos().addAll(Arrays.asList(produto2));
+
+        produto1.getCategorias().addAll(Arrays.asList(categoria1));
+        produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+        produto3.getCategorias().addAll(Arrays.asList(categoria1));
+
         categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+        produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
     }
 }
